5e27052ce6a9c379a2392a24904267cc56ee6212
Ivan Petrushev <ivanatora@gmail.com>
2020-04-21 14:45:08 +0300

    expanding corpus

--- gensim_distance.py
+++ gensim_distance.py
@@ -1,15 +1,19 @@
 import os
 import gensim
 import multiprocessing
+from time import time
 # from tensorflow import keras
 # from keras.preprocessing.text import Tokenizer
+
+DATA_DIR = 'data/' # general wiki articles about geography
+# DATA_DIR = 'set-vazov-razkazi/' # Ivan Vazov works
 
 y = []
 x = []
 
-for root, dirs, files in os.walk('data/'):
+for root, dirs, files in os.walk(DATA_DIR):
     for file in files:
-        with open('data/' + file) as fp:
+        with open(DATA_DIR + file) as fp:
             text = fp.read().replace('\n', '')
             x.append(text)
             y.append(file)
@@ -29,10 +33,12 @@ print('Modeling:')
 model = gensim.models.Word2Vec(min_count=1, window=5, size=300, workers=multiprocessing.cpu_count())
 model.build_vocab(x)
 print(model)
-model.train(x, total_examples=model.corpus_count, epochs=100)
-print('Trained', model)
+train_start = time()
+model.train(x, total_examples=model.corpus_count, epochs=30)
+train_duration = time() - train_start
+print('Trained', model, 'in time: ', train_duration)
 
-print('Vocab', list(model.wv.vocab))
+# print('Vocab', list(model.wv.vocab))
 print("Lookup: city")
 print(model.wv.similar_by_word('city'))
 
--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,7 @@
-import requests
+# import requests
+import wikipedia
 import os
+import re
 from time import sleep
 
 wiki_titles = [
@@ -7,17 +9,29 @@ wiki_titles = [
     'Bulgaria', 'Germany', 'Greece', 'Romania', 'Serbia', 'Turkey', 'Albania', 'Andorra', 'Austria', 'Belarus',
     'Belgium', 'Croatia', 'Cyprus', 'Denmark', 'Estonia', 'Finland', 'France', 'Hungary', 'Iceland', 'Ireland',
     'Italy', 'Kosovo', 'Latvia', 'Malta', 'Moldova', 'Monaco', 'Norway', 'Poland', 'Portugal', 'Russia', 'Slovakia',
-    'Slovenia', 'Spain', 'Sweden', 'Ukraine', 'China', 'Japan', 'Vietnam', 'Australia', 'Egypt', 'Brazil', 'Mexico',
+    'Slovenia', 'Spain', 'Sweden', 'Ukraine', 'China', 'Japan', 'Vietnam', 'Egypt', 'Brazil', 'Mexico',
     'Peru', 'Iran', 'Iraq', 'India',
-    # cities
-    'Plovdiv', 'Sofia', 'Burgas', 'Varna', 'Ruse', 'Pleven', 'Sliven', 'Pernik', 'Haskovo', 'Yambol', 'Vratsa',
-    'Gabrovo', 'Asenovgrad', 'Vidin', 'Tarnovo', 'Shumen', 'Sozopol', 'Albena', 'Bansko', 'Pamporovo', 'Borovets',
-    'Nesebar', 'Preslav', 'Ohrid', 'Kazanlak', 'Stara_Zagora', 'Melnik',
-    # geography
-    'Thrace', 'Balkans', 'Danube', 'Rila', 'Pirin', 'Vitosha', 'Musala', 'Iskar', 'Kabile', 'Europe', 'Black_Sea',
-    'Rhodopes', 'Sakar', 'Verila', 'Vihren', 'Kutelo', 'Nile', 'Sahara',
+    # cities - bg
+    'Plovdiv', 'Sofia', 'Burgas', 'Varna', 'Pleven', 'Sliven', 'Pernik', 'Haskovo', 'Yambol', 'Vratsa',
+    'Gabrovo', 'Asenovgrad', 'Vidin', 'Shumen', 'Sozopol', 'Albena', 'Bansko', 'Pamporovo', 'Borovets',
+    'Nesebar', 'Preslav', 'Ohrid', 'Kazanlak', 'Stara_Zagora', 'Melnik', 'Karlovo', 'Kalofer',
+    # cities - world
+    'Tirana', 'Yerevan', 'Vienna', 'Baku', 'Sarajevo', 'Zagreb', 'Prague', 'Copenhagen', 'Helsinki', 'Paris',
+    'Berlin', 'Athens', 'Kiev', 'Minsk', 'Moscow', 'Budapest', 'Dublin', 'Rome', 'Riga', 'Vaduz',
+    'Oslo', 'Lisbon', 'Belgrade', 'Bratislava', 'Madrid', 'Bern', 'Ankara', 'London',
+    # geography - bg
+    'Thrace', 'Balkans', 'Danube', 'Rila', 'Pirin', 'Vitosha', 'Musala', 'Iskar', 'Kabile', 'Black_Sea',
+    'Rhodopes', 'Verila', 'Vihren', 'Kutelo', 'Dospat', 'Maritsa', 'Mesta', 'Osam', 'Rezovo',
+    'Ropotamo', 'Struma', 'Vacha', 'Veleka', 'Thracia',
+    # geography - world
+    'Europe', 'Africa', 'Asia', 'Australia', 'North America', 'South America',
+    'Nile', 'Alps', 'Andes', 'Alaska', 'K2', 'Aconcagua',
+    'Denali', 'Mont_Blanc', 'Amazon_River', 'Lena_River', 'Niger_River', 'Volga', 'Yukon_River',
     # misc
-    'NATO',
+    'European Union', 'Agriculture', 'Christianity', 'Islam', 'Butterfly', 'Deer', 'Wild boar', 'Jackal', 'Fox',
+    'Nuclear power',
+    # history
+    'Roman Empire'
 ]
 
 for title in wiki_titles:
@@ -26,19 +40,29 @@ for title in wiki_titles:
         print('Exists: ' + title)
     else:
         print('Downloading: ' + title)
-        response = requests.get(
-            'https://en.wikipedia.org/w/api.php',
-            params={
-                'action': 'query',
-                'format': 'json',
-                'titles': title,
-                'prop': 'extracts',
-                'exintro': True,
-                'explaintext': True,
-            }
-        )
-        response = response.json()
-        page = next(iter(response['query']['pages'].values()))
+        ## requests-style page fetching
+        # response = requests.get(
+        #     'https://en.wikipedia.org/w/api.php',
+        #     params={
+        #         'action': 'query',
+        #         'format': 'json',
+        #         'titles': title,
+        #         'prop': 'extracts',
+        #         # 'exintro': True,
+        #         # 'exsentences': 10,
+        #         'explaintext': True,
+        #         'exsectionformat': 'plain'
+        #     }
+        # )
+        # response = response.json()
+        # print('response', response)
+        # page = next(iter(response['query']['pages'].values()))
+        # content = page['extract']
+
+        # module-style page fetching
+        content = wikipedia.page(title).content
+        content = re.sub(r'\n=.+?=\n', '', content)
+
         with open(filename, 'w') as fp:
-            fp.write(page['extract'])
+            fp.write(content)
         sleep(1)
